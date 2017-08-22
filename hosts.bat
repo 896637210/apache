e8654470c2425531489b2fb873b4e6cd60a0a7e4
栗悟饭和龟派气功 <896637210@users.noreply.github.com>
2017-08-22 19:07:16 +0800

    Add files via upload

---
+++ hosts.bat
@@ -0,0 +1,10 @@
+attrib -r -h -s C:\windows\system32\drivers\etc\hosts
+
+
+notepad C:\windows\system32\drivers\etc\hosts
+
+
+attrib +r +h +s C:\windows\system32\drivers\etc\hosts
+
+
+ipconfig/flushdns
